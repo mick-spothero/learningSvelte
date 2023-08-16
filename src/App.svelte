bdb02d0fb9d7e801c5a3c33fa756cf921cf5961f
Mick Johnson <mick.johnson@spothero.com>
2023-08-16 13:14:23 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Counter from './lib/Counter.svelte';
+	// let name = 'Svelte';
+	// let src = 'http://picsum.com/200/200';
+	// let string = 'Some <strong>string</strong> with html';
+	let initialCount = 3;
+</script>
+
+<Counter {initialCount} />
+
+<!-- <img {src} alt="Some Text {name}" />
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html string}</p> -->
+
+<style>
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let initialCount = 0;
+	export let maxCount = undefined;
+	let count = initialCount;
+	let string = `You have clicked ${count} times`;
+
+	$: string = `You have clicked ${count} times`; // svelte will add let keyword and curlies
+
+	// $: if (count > 5) {
+	// 	console.log(`The count is ${count}`);
+	// }
+
+	function increment() {
+		if (count === maxCount) {
+			return;
+		}
+		count += 1;
+	}
+</script>
+
+<button on:click={increment}>Clicks {count}</button>
+<h3>{string}</h3>
+
+<style>
+	button {
+		padding: 15px 20px;
+	}
+</style>
