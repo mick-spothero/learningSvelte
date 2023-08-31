39b1ab9032537443e1ac43aa65700a82b135026b
Mick Johnson <mick.johnson@spothero.com>
2023-08-31 16:19:42 -0500

    todo list

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,79 @@
+<svelte:options immutable={true} />
+
 <script>
 	import TodoList from './lib/TodoList.svelte';
+	import { v4 as uuid } from 'uuid';
+
+	let todos = [
+		{
+			id: uuid(),
+			title: 'Todo 1',
+			completed: true
+		},
+		{
+			id: uuid(),
+			title: 'Todo 2',
+			completed: false
+		},
+		{
+			id: uuid(),
+			title: 'Todo 3',
+			completed: false
+		},
+		{
+			id: uuid(),
+			title: 'Todo 4',
+			completed: false
+		}
+	];
+
+	function handleAddTodo(event) {
+		// event.preventDefault()
+		const { detail } = event;
+
+		if (!detail.title) {
+			return;
+		}
+
+		todos = [
+			...todos,
+			{
+				id: uuid(),
+				title: detail.title,
+				completed: false
+			}
+		];
+	}
+
+	function handleRemoveTodo(event) {
+		const { detail } = event;
+
+		if (!detail.id) {
+			return;
+		}
+
+		todos = todos.filter((todo) => todo.id !== detail.id);
+	}
+
+	function handleToggleTodo(event) {
+		const { detail } = event;
+
+		todos = todos.map((todo) => {
+			if (todo.id === detail.id) {
+				return { ...todo, completed: detail.value };
+			} else {
+				return todo;
+			}
+		});
+	}
 </script>
 
-<TodoList />
+<TodoList
+	{todos}
+	on:addtodo={handleAddTodo}
+	on:removetodo={handleRemoveTodo}
+	on:toggletodo={handleToggleTodo}
+/>
 
 <style>
 </style>
--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,1 +1,48 @@
-Todo List
+<script>
+	import Button from './Button.svelte';
+	import { createEventDispatcher } from 'svelte';
+	export let todos = [];
+	let inputText = '';
+	const dispatch = createEventDispatcher();
+
+	function handleAddTodo() {
+		const isNotCancelled = dispatch('addtodo', { title: inputText }, { cancelable: true });
+
+		if (isNotCancelled) {
+			inputText = '';
+		}
+	}
+
+	function handleRemoveTodo(id) {
+		dispatch('removetodo', { id });
+	}
+
+	function handleToggleTodo(id, value) {
+		dispatch('toggletodo', { id, value });
+	}
+</script>
+
+<div class="todo-list-wrapper">
+	<ul>
+		{#each todos as { id, title, completed } (id)}
+			<li>
+				<label>
+					<input
+						on:input={(event) => {
+							event.currentTarget.checked = completed;
+							handleToggleTodo(id, !completed);
+						}}
+						type="checkbox"
+						checked={completed}
+					/>
+					{title}
+				</label>
+				<button on:click={() => handleRemoveTodo(id)}>Remove</button>
+			</li>
+		{/each}
+	</ul>
+	<form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+		<input bind:value={inputText} />
+		<Button disabled={!inputText} type="submit">Add</Button>
+	</form>
+</div>
